cf5485eec8d63bc3ecdc2f3c8702d4ddae861cd5
William Roger <williamroger.frontend@gmail.com>
2018-11-02 06:56:51 -0300

    First Commit

---
+++ src/main/java/br/com/expertsunited/model/JPAUtil/UtilJPA.java
@@ -0,0 +1,25 @@
+package br.com.expertsunited.model.JPAUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class UtilJPA {
+
+	private static EntityManagerFactory emf;
+	
+	public static void createEntityManagerFactory() {
+		if(emf == null)
+		emf = Persistence.createEntityManagerFactory("requestprint");
+	}
+	
+	public static EntityManager getEntityManager() {
+		if(emf == null)
+			createEntityManagerFactory();
+		return emf.createEntityManager();
+	}
+	
+	public static void closeEntityManagerFactory() {
+		emf.close();
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Request Print</h2>
+<h4>Novo Repositório</h4>
+</body>
+</html>
