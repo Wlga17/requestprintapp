64a69d907c732d23c8afd220e9785cdfdbba23f9
William Luiz <williamlga@gmail.com>
2018-11-18 01:10:58 -0200

    Atualização de PropostaMB e da tela de proposta.

--- src/main/java/br/com/expertsunited/controler/managedBean/PropostaMB.java
+++ src/main/java/br/com/expertsunited/controler/managedBean/PropostaMB.java
@@ -3,15 +3,22 @@ package br.com.expertsunited.controler.managedBean;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
 
+import br.com.expertsunited.facade.GraficaFachada;
 import br.com.expertsunited.facade.PropostaFachada;
+import br.com.expertsunited.facade.SolicitacaoFachada;
+import br.com.expertsunited.model.entity.Grafica;
 import br.com.expertsunited.model.entity.Proposta;
+import br.com.expertsunited.model.entity.Solicitacao;
 
 @ManagedBean()
 public class PropostaMB {
 
 	private Proposta proposta;
 	private List<Proposta> listaProposta;
+	private List<SelectItem> grafs;
+	private List<SelectItem> sols;
 	
 	public PropostaMB() {
 		proposta = new Proposta();
@@ -32,7 +39,69 @@ public class PropostaMB {
 	public void setListaProposta(List<Proposta> listaProposta) {
 		this.listaProposta = listaProposta;
 	}
+	
+	/*public List<Grafica> getGrafs() {
+		GraficaFachada fg = new GraficaFachada();
+		try {
+			grafs = fg.getListGrafica();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return grafs;
+	}
 
+	public void setGrafs(List<Grafica> grafs) {
+		this.grafs = grafs;
+	}
+	*/
+	public List<SelectItem> getGrafs() {
+		GraficaFachada fg = new GraficaFachada();
+		try {
+			for (Grafica g : fg.getListGrafica()) {
+			SelectItem sl = new SelectItem(g.getIdUsuario(), g.getNome());	
+			grafs.add(sl);
+			}
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return grafs;
+	}
+
+	public void setGrafs(List<SelectItem> grafs) {
+		this.grafs = grafs;
+	}
+
+	/*public List<Solicitacao> getSols() {
+		SolicitacaoFachada fs = new SolicitacaoFachada();
+		try {
+			sols = fs.getListSolicitacao();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return sols;
+	}
+
+	public void setSols(List<Solicitacao> sols) {
+		this.sols = sols;
+	}
+	*/
+	public List<SelectItem> getSols() {
+		SolicitacaoFachada fs = new SolicitacaoFachada();
+		try {
+			for(Solicitacao s : fs.getListSolicitacao()) {
+				SelectItem sl = new SelectItem(s.getId(), s.getTitulo());
+				sols.add(sl);
+			}
+		}catch (Exception e) {
+			e.getMessage();
+		}
+		return sols;
+	}
+
+	public void setSols(List<SelectItem> sols) {
+		this.sols = sols;
+	}
+	
 	public void inserir() {
 		if(this.proposta != null) {
 			PropostaFachada fachada = new PropostaFachada(); 
