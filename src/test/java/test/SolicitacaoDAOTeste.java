6ee892df35543a68b0b953b798b79db43b1ae9ac
William Roger <williamroger.frontend@gmail.com>
2018-11-20 08:43:55 -0300

    Iniciado Estilização do pagina de Solicitação

--- src/test/java/test/SolicitacaoDAOTeste.java
+++ src/test/java/test/SolicitacaoDAOTeste.java
@@ -3,9 +3,23 @@ package test;
 import org.junit.Test;
 
 import br.com.expertsunited.facade.SolicitacaoFachada;
+import br.com.expertsunited.model.dao.SolicitacaoDAO;
 import br.com.expertsunited.model.entity.Solicitacao;
 
 public class SolicitacaoDAOTeste {
+	
+@Test
+public void createTest() {
+	Solicitacao sol = new Solicitacao();
+	SolicitacaoDAO soliDao = new SolicitacaoDAO();
+	
+	sol.setTitulo("Cartão João Almeida");
+	sol.setDataSolicitacao("12/25/2018");
+	sol.setDataEntrega("30/12/2018");
+	sol.setObservacao("Preciso disso tudo pronto");
+	
+	soliDao.create(sol);
+}
 
 //	@Test
 //	public void getListTest() {
